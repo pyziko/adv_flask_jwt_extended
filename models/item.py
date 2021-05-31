ca4e2a353adfdc50058f3ff656929f763b44d4d5
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-05-31 22:10:14 +0100

    exploring unique columns

--- models/item.py
+++ models/item.py
@@ -7,7 +7,7 @@ class ItemModel(db.Model):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
+    name = db.Column(db.String(80), unique=True)
     price = db.Column(db.Float(precision=2))
     request_id = db.Column(db.String(15))
 
